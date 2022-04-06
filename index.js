32e851bfef7b710d70bd5f2fb68a7184107b9c30
Joel Roed <jooel.roed@gmail.com>
2022-04-07 00:28:29 +0200

    Update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ form.addEventListener('submit',function(event) {
     counterValue += 1;
 
     var activities = document.querySelector('#activities').value
-    document.querySelector(".output__text").innerHTML = counterValue + activities
+    document.querySelector(".output__text").innerHTML += counterValue + ". " + activities + ("<br>")
 
 })
 
