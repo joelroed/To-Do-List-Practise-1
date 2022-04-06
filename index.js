5ea7557244500c0c7e6004c8e5427703fb9c6336
Joel Roed <jooel.roed@gmail.com>
2022-04-07 00:59:29 +0200

    Finished Product

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ form.addEventListener('submit',function(event) {
     event.preventDefault()
     counterValue += 1;
 
-    var activities = document.querySelector('#activities').value
+    var activities = document.querySelector('#activities').value;
     document.querySelector(".output__text").innerHTML += counterValue + ". " + activities + ("<br>")
 
 })
